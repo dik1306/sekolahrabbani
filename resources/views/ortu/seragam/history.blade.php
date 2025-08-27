3a2ce7823863a98cde74d74f6fe9357c078752da
idlofizahir86 <idlofizahir86@gmail.com>
2025-08-28 00:43:28 +0700

    Update Histori seragam

--- resources/views/ortu/seragam/history.blade.php
+++ resources/views/ortu/seragam/history.blade.php
@@ -97,38 +97,42 @@
 
             <div class="tab-pane fade mb-5" id="nav-jersey" role="tabpanel" aria-labelledby="nav-jersey-tab" tabindex="0">
                 @foreach ($order_jersey as $item_jersey)
-                    <a href="{{ route('jersey.history.detail', ['id' => $item_jersey->no_pesanan]) }}" style="text-decoration: none">
-                        <div class="card card-history mt-3">
-                            <div class="card-header d-flex" style="justify-content: space-between; font-size: 12px">
-                                <span class=""> No Pesanan </span>
-                                <span > {{$item_jersey->no_pesanan}} </span>
-                            </div>
-                            <div class="card-body d-flex">
-                                <div class="frame-bayar">
-                                    <img src="{{asset('storage/'.$item_jersey->image_1)}}" width="100%" style="height: 100%; object-fit:cover; border-radius:1rem">
+                    @if($item_jersey->no_pesanan)\\
+                        <a href="{{ route('jersey.history.detail', ['id' => $item_jersey->no_pesanan]) }}" style="text-decoration: none">
+                            <div class="card card-history mt-3">
+                                <div class="card-header d-flex" style="justify-content: space-between; font-size: 12px">
+                                    <span class=""> No Pesanan </span>
+                                    <span > {{$item_jersey->no_pesanan}} </span>
                                 </div>
-                                <div class="d-flex mx-2">
-                                    <div class="" style="width: 150px">
-                                        <p class="mb-0" style="font-size: 14px;"> {{$item_jersey->nama_jersey}} </p>
-                                        <p class="mb-0 price-diskon"> <b> Rp. {{number_format((($item_jersey->total_harga))) }} </b> </p>
-                                        <p class="mb-0" style="font-size: 8px">Waktu Pesan: {{$item_jersey->created_at}} </p>
-                
+                                <div class="card-body d-flex">
+                                    <div class="frame-bayar">
+                                        <img src="{{asset('storage/'.$item_jersey->image_1)}}" width="100%" style="height: 100%; object-fit:cover; border-radius:1rem">
+                                    </div>
+                                    <div class="d-flex mx-2">
+                                        <div class="" style="width: 150px">
+                                            <p class="mb-0" style="font-size: 14px;"> {{$item_jersey->nama_jersey}} </p>
+                                            <p class="mb-0 price-diskon"> <b> Rp. {{number_format((($item_jersey->total_harga))) }} </b> </p>
+                                            <p class="mb-0" style="font-size: 8px">Waktu Pesan: {{$item_jersey->created_at}} </p>
+                                            
+                                        </div>
+                                    </div>
+                                    <div class="status" style="margin-left: auto">
+                                        @if ($item_jersey->status == 'success')
+                                        <span class="badge bg-success" style="font-size: 12px"> {{$item_jersey->status}} </span>
+                                        
+                                        @elseif($item_jersey->status == 'pending')
+                                        <span class="badge bg-warning" style="font-size: 12px"> Menunggu </span>
+                                        
+                                        @elseif($item_jersey->status == 'cancel' || $item_jersey->status == 'failed' || $item_jersey->status == 'expired' )
+                                        <span class="badge bg-danger" style="font-size: 12px"> Gagal </span>
+                                        @endif
                                     </div>
                                 </div>
-                                <div class="status" style="margin-left: auto">
-                                    @if ($item_jersey->status == 'success')
-                                        <span class="badge bg-success" style="font-size: 12px"> {{$item_jersey->status}} </span>
-        
-                                    @elseif($item_jersey->status == 'pending')
-                                        <span class="badge bg-warning" style="font-size: 12px"> Menunggu </span>
-        
-                                    @elseif($item_jersey->status == 'cancel' || $item_jersey->status == 'failed' || $item_jersey->status == 'expired' )
-                                        <span class="badge bg-danger" style="font-size: 12px"> Gagal </span>
-                                    @endif
-                                </div>
                             </div>
-                        </div>
-                    </a>
+                        </a>
+                    @else
+                        <p>No pesanan tidak tersedia</p> <!-- Menampilkan pesan jika no_pesanan kosong -->
+                    @endif
                 @endforeach
             </div>
            
